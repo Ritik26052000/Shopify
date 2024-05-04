ae02d2ba533ac44f1757f0461b01aca1298d5be4
Ritik26052000 <sonwanianshu2000@gmail.com>
2024-05-04 23:32:10 +0530

    Shopify assignment

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      color:{
+          'black':'#000000',
+          'light_black': '#212326'
+      },
+    },
+  },
+  plugins: [],
+}
+
